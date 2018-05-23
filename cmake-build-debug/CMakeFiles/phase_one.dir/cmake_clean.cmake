f6699f049241abb227166de89998e9d0955c725f
Mahin Mirshams <m.mirshams@aut.ac.ir>
2018-05-23 16:54:44 +0430

    first commit

---
+++ cmake-build-debug/CMakeFiles/phase_one.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phase_one.dir/main.cpp.o"
+  "phase_one.pdb"
+  "phase_one.exe"
+  "libphase_one.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phase_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
