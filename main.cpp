1aa7376122665eab986810ec7fd2d59bdaba063e
saeedehsp <s.sadeghpour97@gmail.com>
2018-05-23 17:20:21 +0430

    add functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "stdio.h"
+using namespace std;
+
+
+int message_length (string input){
+    return static_cast<int>(input.length() * 8);
+}
+
+
+int calculate_zero(int m){
+
+    int i = 0;
+
+    while(!(m+1 % 448 == 0)){
+        i++;
+    }
+    return i;
+
+}
+
+int padding_and_parsing (int l){
+    int k = calculate_zero(l+1);
+
+    int out = (l + 1 + );
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
     return 0;
 }
+
