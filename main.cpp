3143e361fe6ca11dcfd9248f1e86a9bf6d39d6ff
saeedehsp <s.sadeghpour97@gmail.com>
2018-06-06 15:34:19 +0430

    compression improved

--- main.cpp
+++ main.cpp
@@ -25,6 +25,16 @@ string *H5;
 string *H6;
 string *H7;
 
+uint k[64] = {
+        0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+        0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+        0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+        0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+        0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+        0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+        0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+        0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+};
 
 
 int calculate_expand(string input){
@@ -168,7 +178,7 @@ string sigma(string x , int num){
     return  output ;
 }
 
-void expansion(string input  ){
+void expansion(string input){
     int i = 0;
     // create 16 blocks(32 bits) from 512 bit
     for(int k = 0 ; k < 16 ; k+=32 ){
@@ -212,6 +222,11 @@ void init(){
     H5[0] = F;
     H6[0] = G;
     H7[0] = H;
+
+
+
+
+
 }
 
 string my_xor (string a , string b){
@@ -322,10 +337,16 @@ void  hash(string w, string k){
     D = C;
     B = A;
     G = F;
-    E = D + t1;
+    E = add(D , t1);
     C = B;
     A = subtract(multiply(t1,3), t2);
 }
+
+void concatination(string *h, int N){
+    h[N] = H0[N] + H1[N] + H2[N] + H3[N] + H4[N] + H5[N] + H6[N] + H7[N];
+}
+
+
 
 int main() {
     string input = "abc";
