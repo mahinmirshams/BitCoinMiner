9481af0b2c764ee804268184492ad018395de4b2
Mahin Mirshams <m.mirshams@aut.ac.ir>
2018-07-06 19:59:43 +0430

    compression updated

--- VHDL/compression.vhd
+++ VHDL/compression.vhd
@@ -4,105 +4,94 @@ use work.pkg.all;
 use ieee.numeric_std.all;
 
 
-PACKAGE MyType IS type array2d_64_32 is array (63 downto 0) of unsigned(31 downto 0);
+PACKAGE MyType IS 
+    type array2d_64_32 is array (63 downto 0) of std_logic_vector(31 downto 0);
+    type array2d_8_32 is array (0 to 7) of std_logic_vector(31 downto 0);
  END PACKAGE MyType; 
 
 
 entity compression is
     Port ( W , K : in MyType;
-           ready: in std_logic;
-           Hn :  out unsigned(255 downto 0));
+           ready  , en , rst: in std_logic;
+           Hi : in array2d_8_32;
+           Hn :  out array2d_8_32);
 end compression;
 
 architecture RTL of compression is
 
-signal H0_A  : unsigned(31 downto 0) := x"6a09e667";
-signal H1_B  : unsigned(31 downto 0) := x"bb67ae85";
-signal H2_C  : unsigned(31 downto 0) := x"3c6ef372";
-signal H3_D  : unsigned(31 downto 0) := x"a54ff53a";
-signal H4_E : unsigned(31 downto 0) := x"510e527f";
-signal H5_F  : unsigned(31 downto 0) := x"9b05688c";
-signal H6_G  : unsigned(31 downto 0) := x"1f83d9ab";
-signal H7_H  : unsigned(31 downto 0) := x"5be0cd19";
+signal A  : std_logic_vector(31 downto 0) := x"6a09e667";
+signal B  : std_logic_vector(31 downto 0) := x"bb67ae85";
+signal C  : std_logic_vector(31 downto 0) := x"3c6ef372";
+signal D  : std_logic_vector(31 downto 0) := x"a54ff53a";
+signal Ei : std_logic_vector(31 downto 0) := x"510e527f";
+signal F  : std_logic_vector(31 downto 0) := x"9b05688c";
+signal G  : std_logic_vector(31 downto 0) := x"1f83d9ab";
+signal H  : std_logic_vector(31 downto 0) := x"5be0cd19";
 
 
-function ch (x, y, z : unsigned(31 downto 0) ) return unsigned is
-	variable temp1: unsigned(31 downto 0);
-	variable temp2: unsigned(31 downto 0);
-	variable temp3: unsigned(31 downto 0);
+function ch (x, y, z : std_logic_vector(31 downto 0) ) return std_logic_vector is
 begin
-    temp1 := x and y;
-    temp2 := not(y) and z;
-    temp3 := not(x) and z;
-	return temp1 xor temp2 xor temp3;
+    return (x and y) xor ((not y) and z) xor ((not x) and z);
 end ch;
 
-function maj (x, y, z : unsigned(31 downto 0) ) return unsigned is
-	variable temp1: unsigned(31 downto 0);
-	variable temp2: unsigned(31 downto 0);
-	variable temp3: unsigned(31 downto 0);
+function maj (x, y, z : std_logic_vector(31 downto 0) ) return std_logic_vector is
 begin
-    temp1 := x and z;
-    temp2 := x and y;
-    temp3 := y and z;
-	return temp1 xor temp2 xor temp3;
+    return (x and y) xor (x and z) xor (y and z);
 end maj;
 
-function sigma0 (x : unsigned(31 downto 0) ) return unsigned is
-	variable temp1: unsigned(31 downto 0);
-	variable temp2: unsigned(31 downto 0);
-	variable temp3: unsigned(31 downto 0);
-	variable temp4: unsigned(31 downto 0);
+function sigma0 (x : std_logic_vector(31 downto 0) ) return std_logic_vector is
 begin
-    temp1 := x ror 2;
-    temp2 := x ror 13;
-    temp3 := x ror 22;
-    temp4 := x srl 7;
-	return temp1 xor temp2 xor temp3 xor temp4;
+return std_logic_vector((x ror 2) xor (x ror 13) xor (x ror 22) xor (x srl 7));
 end sigma0;
 
-function sigma1 (x : unsigned(31 downto 0) ) return unsigned is
-	variable temp1: unsigned(31 downto 0);
-	variable temp2: unsigned(31 downto 0);
-	variable temp3: unsigned(31 downto 0);
+function sigma1 (x : std_logic_vector(31 downto 0) ) return std_logic_vector is
 begin
-    temp1 := x ror 6;
-    temp2 := x ror 11;
-    temp3 := x ror 25;
-	return temp1 xor temp2 xor temp3;
+    return std_logic_vector((x ror 6) xor (x ror 11) xor ( x ror 25));
 end sigma1;
 
-function sigma2 (x : unsigned(31 downto 0) ) return unsigned is
-	variable temp1: unsigned(31 downto 0);
-	variable temp2: unsigned(31 downto 0);
-	variable temp3: unsigned(31 downto 0);
-	Variable temp4: unsigned(31 downto 0);
+function sigma2 (x : std_logic_vector(31 downto 0) ) return std_logic_vector is
 begin
-    temp1 := x ror 2;
-    temp2 := x ror 3;
-    temp3 := x ror 15;
-    emp4 := x srl 5;
-	return temp1 xor temp2 xor temp3 xor temp4;
+    return std_logic_vector((x ror 2) xor (x ror 3) xor (x ror 15) xor (x srl 5));
 end sigma2;
 
-signal temp1, temp2 : unsigned(31 downto 0);
+signal temp1, temp2 : std_logic_vector(31 downto 0);
 begin
 
-process( ready)
+process( ready )
 begin 
-    if (ready = '1') then
+    if (en = '1') then
+    if(rst = '1')then
+             A <= x"6a09e667";
+             B  <= x"bb67ae85";
+             C  <= x"3c6ef372";
+             D  <= x"a54ff53a";
+             Ei <= x"510e527f";
+             F  <= x"9b05688c";
+             G  <= x"1f83d9ab";
+             H  <= x"5be0cd19";
+    elsif (ready = '1') then
      for i in 0 to 63 loop
-         temp2 <= H7_H + sigma1(H4_E) + ch(H4_E, f, g) + K(i) + W(i);
-         temp1 <= sigma0(H0_A) + maj(H0_A , H1_B , H2_C) + sigma2(H2_C + H3_D);
-         H7_H <= H6_G;
-         H5_F <= H4_E;
-         H3_D <= H2_C;
-         H1_B <= H0_A;
-         H6_G <= H5_F;
-         H4_E <= H3_D + temp1;
-         H2_C <= H1_B;
-         H0_A <= temp1 + temp1 + temp1 - temp2;
+         temp2 <= H + sigma1(Ei) + ch(Ei, F, G) + K(i) + W(i);
+         temp1 <= sigma0(A) + maj(A , B , C) + sigma2(C + D);
+         H <= G;
+         F <= Ei;
+         D <= C;
+         B <= A;
+         G <= F;
+         Ei <= D + temp1;
+         C <= B;
+         A <= temp1 + temp1 + temp1 - temp2;
      end loop;
+     Hn(0) <= A  + Hi(0); 
+     Hn(1) <= B + Hi(1);
+     Hn(2) <= C  + Hi(2); 
+     Hn(3) <= D + Hi(3); 
+     Hn(4) <= Ei + Hi(4); 
+     Hn(5) <= F + Hi(5);
+     Hn(6) <= G  + Hi(6); 
+     Hn(7) <= H + Hi(7); 
+     
+ end if;
  end if;
 end process;
 
